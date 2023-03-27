d3939f922b11a9b73f8898b6a05c2fae0165615e
Camilo Carabali <73615948+camiloCarabali@users.noreply.github.com>
2023-03-27 15:40:20 -0500

    Creacion del crud de empleados y de empresas

--- servidor/index.js
+++ servidor/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-})
+conectarDB()
+
+app.use(express.json());
+
+app.use('/api/empresas', require('./routes/empresa'));
+
+app.use('/api/empleados', require('./routes/empleado'));
 
 app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente')
